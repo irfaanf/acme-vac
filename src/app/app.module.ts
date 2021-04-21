fe886b3e8515d33a6aaba1d512f258dd4143c249
Irfaan Fakir <irfaan@hotmail.co.za>
2021-04-21 11:44:48 +0200

    Created top navigation component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddPersonScreenComponent } from './add-person-screen/add-person-screen.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddPersonScreenComponent } from './add-person-screen/add-person-screen.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
 import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, AddPersonScreenComponent, TopNavigationComponent],
+  declarations: [
+    AppComponent,
+    AddPersonScreenComponent,
+    TopNavigationComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
