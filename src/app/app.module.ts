566f57ab4dba929c41524c429e59cb3fc8b496ab
Irfaan Fakir <irfaan@hotmail.co.za>
2021-04-23 14:15:41 +0200

    Add Flex Layout module to app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,11 +10,11 @@ import { AppComponent } from './app.component';
 import { BodyComponent } from './web/body/body.component';
 import { AddPersonScreenComponent } from './web/body/content/add-person-screen/add-person-screen.component';
 import { ContentComponent } from './web/body/content/content.component';
+import { DesktopSecondaryNavigationComponent } from './web/body/secondary-navigation/desktop-secondary-navigation/desktop-secondary-navigation.component';
+import { MobileSecondaryNavigationComponent } from './web/body/secondary-navigation/mobile-secondary-navigation/mobile-secondary-navigation.component';
 import { SecondaryNavigationComponent } from './web/body/secondary-navigation/secondary-navigation.component';
 import { TopNavigationComponent } from './web/top-navigation/top-navigation.component';
 import { WebComponent } from './web/web.component';
-import { MobileSecondaryNavigationComponent } from './web/body/secondary-navigation/mobile-secondary-navigation/mobile-secondary-navigation.component';
-import { DesktopSecondaryNavigationComponent } from './web/body/secondary-navigation/desktop-secondary-navigation/desktop-secondary-navigation.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { DesktopSecondaryNavigationComponent } from './web/body/secondary-naviga
     MatToolbarModule,
     MatIconModule,
     AngularMaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
