1603ff106d35ed14e78639f06702a107050b330c
Irfaan Fakir <irfaan@hotmail.co.za>
2021-04-27 15:53:32 +0200

    Install ngx-mask input mask library.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +17,8 @@ import { MobileSecondaryNavigationComponent } from './web/body/secondary-navigat
 import { SecondaryNavigationComponent } from './web/body/secondary-navigation/secondary-navigation.component';
 import { TopNavigationComponent } from './web/top-navigation/top-navigation.component';
 import { WebComponent } from './web/web.component';
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
   declarations: [
@@ -36,8 +40,10 @@ import { WebComponent } from './web/web.component';
     MatIconModule,
     AngularMaterialModule,
     FlexLayoutModule,
+    FormsModule,
+    NgxMaskModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
