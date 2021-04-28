24e137cbf2d6939006f3212135679aff4e697541
Irfaan Fakir <irfaan@hotmail.co.za>
2021-04-28 22:31:17 +0200

    Added submit button, bound name field to patient object.

--- src/app/web/body/content/add-person-screen/add-person-screen.component.ts
+++ src/app/web/body/content/add-person-screen/add-person-screen.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Patient } from 'src/app/model/patient/patient';
+import { PatientService } from 'src/app/service/patient/patient.service';
 
 
 @Component({
@@ -8,12 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPersonScreenComponent implements OnInit {
 
+  patient: Patient;
+
   startDate: Date;
 
-  constructor() { }
+  constructor(private patientService: PatientService, private router: Router) { 
+    this.patient = new Patient();
+  }
 
   ngOnInit(): void {
     this.startDate = new Date(1930, 0, 1);
+  }
+
+  onSubmit() {
+    this.patientService.save(this.patient).subscribe(result => {
+      this.router.navigate(['/patients']);
+    })
   }
 
 }
