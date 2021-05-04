362cc54a91246928738591be4db103ac136c88bf
Irfaan Fakir <irfaan@hotmail.co.za>
2021-05-04 12:35:00 +0200

    Make "Manage" links point to new Manage All component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddPersonScreenComponent } from './web/body/content/add-person-screen/add-person-screen.component';
 import { DashboardComponent } from './web/body/content/dashboard/dashboard.component';
+import { ManageAllPatientsComponent } from './web/body/content/manage-all-patients/manage-all-patients.component';
 import { ManagePatientComponent } from './web/body/content/manage-patient/manage-patient.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'patients', component: AddPersonScreenComponent },
+  { path: 'manage-all', component: ManageAllPatientsComponent },
   { path: 'manage', component: ManagePatientComponent },
 ];
 
